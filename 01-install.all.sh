458b74f80d965bd4f86780033ff2bc71ac8b98f7
Rafael Pezzetti <rafael.pezzetti@gmail.com>
2020-04-27 14:39:07 -0300

    fea: install robo3t and create desktop shorcut

--- 01-install.all.sh
+++ 01-install.all.sh
@@ -128,6 +128,31 @@ chmod 700 get_helm.sh
 helm version
 rm -rf get_helm.sh
 
+echo "########## <installing robo3t> ##########" 
+wget -c https://download-test.robomongo.org/linux/robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+sudo tar -xvzf robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+mkdir /usr/local/bin/robo3t
+sudo mv robo3t-1.3.1-linux-x86_64-7419c406/*  /usr/local/bin/robo3t
+rm -rf robo3t-1.3.1-linux-x86_64-7419c406.tar.gz
+rm -rf robo3t-1.3.1-linux-x86_64-7419c406
+sudo chmod +x -R /usr/local/bin/robo3t
+cd /usr/local/bin/robo3t/bin
+
+echo "########## <creating robo3t shortcut> ##########"
+wget -c https://dashboard.snapcraft.io/site_media/appmedia/2018/09/logo-256x256.png -O icon.png
+touch robo3t.desktop
+sudo echo "[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=Robo3t
+Icon=/usr/local/bin/robo3t/bin/icon.png
+Exec=\"/usr/local/bin/robo3t/bin/robo3t\"
+Comment=Robo3t 
+Categories=Development;
+Terminal=false
+StartupNotify=true" >> robo3t.desktop
+sudo mv robo3t.desktop /usr/share/applications/robo3t.desktop
+
 echo '########## <installing zsh> ##########'
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
