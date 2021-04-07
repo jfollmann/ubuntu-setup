b4ea56aade6f89d19bfe5af00f3941e648c3a360
Jefferson Follmann <jeff.follmann@gmail.com>
2021-04-07 14:11:24 -0300

    Update 02-personal.settings.sh

--- 02-personal.settings.sh
+++ 02-personal.settings.sh
@@ -62,8 +62,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-source ~/.bashrc
-source ~/.zshrc
+#source ~/.zshrc
 
 nvm --version
 nvm install 14
