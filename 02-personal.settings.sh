11fa92904b5677d9aa32bd9b5d3232ec69c9807f
Jefferson Follmann <jeff.follmann@gmail.com>
2021-07-07 23:21:48 -0300

    Update setup settings

--- 02-personal.settings.sh
+++ 02-personal.settings.sh
@@ -54,7 +54,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 # export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-source ~/.bashrc
+# source ~/.bashrc
 
 nvm --version
 nvm install 14
@@ -63,7 +63,7 @@ nvm use default
 node --version
 npm --version
 
-source ~/.bashrc
+# source ~/.bashrc
 
 echo '########## <installing npm global modules> ##########'
 npm install -g fkill-cli
