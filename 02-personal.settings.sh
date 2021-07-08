2d8267a59f5ac6ee752af67f822848fd51ed8377
Jefferson Follmann <jeff.follmann@gmail.com>
2021-07-07 23:02:33 -0300

    Update setup settings

--- 02-personal.settings.sh
+++ 02-personal.settings.sh
@@ -51,8 +51,8 @@ git clone https://github.com/jfollmann/docker-composes.git ~/Projects/docker-com
 
 echo '########## <installing nvm> ##########'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 source ~/.zshrc
 
